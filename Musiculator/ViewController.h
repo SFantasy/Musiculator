74a6b60153420bd126cdec2d839e1d9d9d096ecb
Fantasy Shao <forever.fantasy27@gmail.com>
2014-01-14 23:50:33 +0800

    添加了按钮的边框，更改了=号按钮的大小

--- Musiculator/ViewController.h
+++ Musiculator/ViewController.h
@@ -10,4 +10,44 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *calculateField;
+@property (weak, nonatomic) IBOutlet UIButton *zeroBtn;
+- (IBAction)typeZero:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *oneBtn;
+- (IBAction)typeOne:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *twoBtn;
+- (IBAction)typeTwo:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *threeBtn;
+- (IBAction)typeThree:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *fourBtn;
+- (IBAction)typeFour:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *fiveBtn;
+- (IBAction)typeFive:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *sixBtn;
+- (IBAction)typeSix:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *sevenBtn;
+- (IBAction)typeSeven:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *eightBtn;
+- (IBAction)typeEight:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *nineBtn;
+- (IBAction)typeNine:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *plusBtn;
+- (IBAction)typePlus:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *minusBtn;
+- (IBAction)typeMinus:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *equalBtn;
+- (IBAction)typeEqual:(id)sender;
+
 @end
--- Musiculator/ViewController.m
+++ Musiculator/ViewController.m
@@ -17,7 +17,50 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // TODO 将输入框设置为只读
+    [_calculateField setPlaceholder:@"Result here"];
+    UIColor *textColor = _zeroBtn.titleLabel.textColor;
+    // 设置边框
+    _zeroBtn.layer.cornerRadius  = _zeroBtn.bounds.size.width / 2.0;
+    _oneBtn.layer.cornerRadius   = _oneBtn.bounds.size.width / 2.0;
+    _twoBtn.layer.cornerRadius   = _twoBtn.bounds.size.width / 2.0;
+    _threeBtn.layer.cornerRadius = _threeBtn.bounds.size.width / 2.0;
+    _fourBtn.layer.cornerRadius  = _fourBtn.bounds.size.width / 2.0;
+    _fiveBtn.layer.cornerRadius  = _fiveBtn.bounds.size.width / 2.0;
+    _sixBtn.layer.cornerRadius   = _sixBtn.bounds.size.width / 2.0;
+    _sevenBtn.layer.cornerRadius = _sevenBtn.bounds.size.width / 2.0;
+    _eightBtn.layer.cornerRadius = _eightBtn.bounds.size.width / 2.0;
+    _nineBtn.layer.cornerRadius  = _nineBtn.bounds.size.width / 2.0;
+    _plusBtn.layer.cornerRadius  = _plusBtn.bounds.size.width / 2.0;
+    _minusBtn.layer.cornerRadius = _minusBtn.bounds.size.width / 2.0;
+    // 设置边框颜色
+    _zeroBtn.layer.borderColor  = textColor.CGColor;
+    _oneBtn.layer.borderColor   = textColor.CGColor;
+    _twoBtn.layer.borderColor   = textColor.CGColor;
+    _threeBtn.layer.borderColor = textColor.CGColor;
+    _fourBtn.layer.borderColor  = textColor.CGColor;
+    _fiveBtn.layer.borderColor  = textColor.CGColor;
+    _sixBtn.layer.borderColor   = textColor.CGColor;
+    _sevenBtn.layer.borderColor = textColor.CGColor;
+    _eightBtn.layer.borderColor = textColor.CGColor;
+    _nineBtn.layer.borderColor  = textColor.CGColor;
+    _plusBtn.layer.borderColor  = textColor.CGColor;
+    _minusBtn.layer.borderColor = textColor.CGColor;
+    _equalBtn.layer.borderColor = textColor.CGColor;
+    // 设置边框宽度
+    _zeroBtn.layer.borderWidth  = 1.0;
+    _oneBtn.layer.borderWidth   = 1.0;
+    _twoBtn.layer.borderWidth   = 1.0;
+    _threeBtn.layer.borderWidth = 1.0;
+    _fourBtn.layer.borderWidth  = 1.0;
+    _fiveBtn.layer.borderWidth  = 1.0;
+    _sixBtn.layer.borderWidth   = 1.0;
+    _sevenBtn.layer.borderWidth = 1.0;
+    _eightBtn.layer.borderWidth = 1.0;
+    _nineBtn.layer.borderWidth  = 1.0;
+    _plusBtn.layer.borderWidth  = 1.0;
+    _minusBtn.layer.borderWidth = 1.0;
+    _equalBtn.layer.borderWidth = 1.0;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +69,30 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)typeZero:(id)sender {
+}
+- (IBAction)typeOne:(id)sender {
+}
+- (IBAction)typeTwo:(id)sender {
+}
+- (IBAction)typeThree:(id)sender {
+}
+- (IBAction)typeFour:(id)sender {
+}
+- (IBAction)typeFive:(id)sender {
+}
+- (IBAction)typeSix:(id)sender {
+}
+- (IBAction)typeSeven:(id)sender {
+}
+- (IBAction)typeEight:(id)sender {
+}
+- (IBAction)typeNine:(id)sender {
+}
+- (IBAction)typePlus:(id)sender {
+}
+- (IBAction)typeMinus:(id)sender {
+}
+- (IBAction)typeEqual:(id)sender {
+}
 @end
