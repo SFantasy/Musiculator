9f4dd013acbc6cd6419a6f262f19cd0a4a149fc4
Fantasy Shao <forever.fantasy27@gmail.com>
2014-01-29 20:09:57 +0800

    [update] A little bit optimization.

--- Musiculator/ViewController.m
+++ Musiculator/ViewController.m
@@ -26,57 +26,35 @@
     AudioServicesPlaySystemSound(soundID);
 }
 
+- (void)setButtonStyle : (UIButton*) btnName
+{
+    UIColor *textColor = _zeroBtn.titleLabel.textColor;
+    // 设置边框圆角，颜色，宽度
+    btnName.layer.cornerRadius = btnName.bounds.size.width / 2.0;
+    btnName.layer.borderColor  = textColor.CGColor;
+    btnName.layer.borderWidth  = 1.0;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // 设置输入框
     [_calculateField setPlaceholder:@"Result here"];
     _calculateField.enabled = NO;
-    UIColor *textColor = _zeroBtn.titleLabel.textColor;
-    // 设置边框
-    _zeroBtn.layer.cornerRadius  = _zeroBtn.bounds.size.width / 2.0;
-    _oneBtn.layer.cornerRadius   = _oneBtn.bounds.size.width / 2.0;
-    _twoBtn.layer.cornerRadius   = _twoBtn.bounds.size.width / 2.0;
-    _threeBtn.layer.cornerRadius = _threeBtn.bounds.size.width / 2.0;
-    _fourBtn.layer.cornerRadius  = _fourBtn.bounds.size.width / 2.0;
-    _fiveBtn.layer.cornerRadius  = _fiveBtn.bounds.size.width / 2.0;
-    _sixBtn.layer.cornerRadius   = _sixBtn.bounds.size.width / 2.0;
-    _sevenBtn.layer.cornerRadius = _sevenBtn.bounds.size.width / 2.0;
-    _eightBtn.layer.cornerRadius = _eightBtn.bounds.size.width / 2.0;
-    _nineBtn.layer.cornerRadius  = _nineBtn.bounds.size.width / 2.0;
-    _plusBtn.layer.cornerRadius  = _plusBtn.bounds.size.width / 2.0;
-    _minusBtn.layer.cornerRadius = _minusBtn.bounds.size.width / 2.0;
-    _clearBtn.layer.cornerRadius = _clearBtn.bounds.size.width / 2.0;
-    // 设置边框颜色
-    _zeroBtn.layer.borderColor  = textColor.CGColor;
-    _oneBtn.layer.borderColor   = textColor.CGColor;
-    _twoBtn.layer.borderColor   = textColor.CGColor;
-    _threeBtn.layer.borderColor = textColor.CGColor;
-    _fourBtn.layer.borderColor  = textColor.CGColor;
-    _fiveBtn.layer.borderColor  = textColor.CGColor;
-    _sixBtn.layer.borderColor   = textColor.CGColor;
-    _sevenBtn.layer.borderColor = textColor.CGColor;
-    _eightBtn.layer.borderColor = textColor.CGColor;
-    _nineBtn.layer.borderColor  = textColor.CGColor;
-    _plusBtn.layer.borderColor  = textColor.CGColor;
-    _minusBtn.layer.borderColor = textColor.CGColor;
-    _equalBtn.layer.borderColor = textColor.CGColor;
-    _clearBtn.layer.borderColor = textColor.CGColor;
-    // 设置边框宽度
-    _zeroBtn.layer.borderWidth  = 1.0;
-    _oneBtn.layer.borderWidth   = 1.0;
-    _twoBtn.layer.borderWidth   = 1.0;
-    _threeBtn.layer.borderWidth = 1.0;
-    _fourBtn.layer.borderWidth  = 1.0;
-    _fiveBtn.layer.borderWidth  = 1.0;
-    _sixBtn.layer.borderWidth   = 1.0;
-    _sevenBtn.layer.borderWidth = 1.0;
-    _eightBtn.layer.borderWidth = 1.0;
-    _nineBtn.layer.borderWidth  = 1.0;
-    _plusBtn.layer.borderWidth  = 1.0;
-    _minusBtn.layer.borderWidth = 1.0;
-    _equalBtn.layer.borderWidth = 1.0;
-    _clearBtn.layer.borderWidth = 1.0;
+    [self setButtonStyle:_zeroBtn];
+    [self setButtonStyle:_oneBtn];
+    [self setButtonStyle:_twoBtn];
+    [self setButtonStyle:_threeBtn];
+    [self setButtonStyle:_fourBtn];
+    [self setButtonStyle:_fiveBtn];
+    [self setButtonStyle:_sixBtn];
+    [self setButtonStyle:_sevenBtn];
+    [self setButtonStyle:_eightBtn];
+    [self setButtonStyle:_nineBtn];
+    [self setButtonStyle:_plusBtn];
+    [self setButtonStyle:_minusBtn];
+    [self setButtonStyle:_clearBtn];
+    [self setButtonStyle:_equalBtn];
 }
 
 - (void)didReceiveMemoryWarning
